660cfad70faa89020ea18f7471fb6e3a55715f6e
Andrew Bespaly <32086172+andrewbespaly@users.noreply.github.com>
2020-11-01 23:11:45 -0800

    new react app setup ran, components folder created, homepage app created

---
+++ nba-dashboard/src/components/homepage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+export default class homepage extends Component {
+
+  render() {
+    return (
+      <div>
+          <h1>
+            NBA Dashboard
+          </h1>
+
+        
+      </div>
+    );
+  }
+}
